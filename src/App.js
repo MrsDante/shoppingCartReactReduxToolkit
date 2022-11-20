d57421c4af1bbb7de9f2c0ac3cadb8c381dfaf41
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-20 20:48:58 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-d
 import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/cart" element={<Cart />} />
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />}
+    />
         </Routes>
       </Router>
     </div>
