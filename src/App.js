0699e0396b3f0c25d4d29aeea0aa67c9a35c937f
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-22 18:54:44 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 
+import { ToastContainer } from 'react-toastify';
+
 function App() {
   return (
     <div className="App">
       <Router>
+      <ToastContainer />
         <NavBar />
         <Routes>
           <Route path="/cart" element={<Cart />} />
--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 
 const initialState = {
@@ -16,9 +17,15 @@ const cartSlice = createSlice({
 
         if (itemIndex >= 0) {
             state.cartItems[itemIndex].cardQuabtity += 1;
+            toast.info('increased product quantity', {
+                position: 'bottom-left',
+            })
         } else {
         const tempProduct = {...action.payload, cardQuabtity: 1};
         state.cartItems.push(tempProduct);
+        toast.success('added a new product to cart', {
+            position: 'bottom-left',
+        })
         }
       },
   },
