83cee7c668a196d0604eff574279b4e266ea457e
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-22 20:51:06 +0300

    .

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,7 +6,7 @@ const Cart = () => {
   const cart = useSelector(state => state.cart);
 console.log(cart)
   return (
-    <div className="card-container">
+    <div className="cart-container">
       <h2>Shopping Cart</h2>
       { cart.cartItems.length === 0 ? (
         <div className="cart-empty">
